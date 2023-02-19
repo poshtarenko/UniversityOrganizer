3047bf342cad57a3c20f4365f8d3e6a6725e05d6
poshtarenko <catemperor228@gmail.com>
2023-02-19 16:53:10 +0200

    init

--- src/main/java/com/nyanband/university_organizer/repository/ddl.sql
+++ src/main/java/com/nyanband/university_organizer/repository/ddl.sql
@@ -1,26 +1,26 @@
 CREATE TABLE Course
 (
-    id  BIGSERIAL PRIMARY KEY,
-    num int,
-    user_id REFERENCES User ON DELETE CASCADE ON UPDATE CASCADE
+    id      BIGSERIAL PRIMARY KEY,
+    num     int,
+    user_id INTEGER REFERENCES User ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE Semester
 (
-    id  BIGSERIAL PRIMARY KEY,
-    num int,
-    course_id REFERENCES Course ON DELETE CASCADE ON UPDATE CASCADE
+    id        BIGSERIAL PRIMARY KEY,
+    num       int,
+    course_id INTEGER REFERENCES Course ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE Discipline
 (
     id              BIGSERIAL PRIMARY KEY,
     discipline_name varchar(128),
-    semester_id REFERENCES Semester ON DELETE CASCADE ON UPDATE CASCADE
+    semester_id     INTEGER REFERENCES Semester ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE Folder
 (
-    id          BIGSERIAL PRIMARY KEY,
-    folder_name varchar(128),
-    discipline_id REFERENCES Discipline ON DELETE CASCADE ON UPDATE CASCADE
+    id            BIGSERIAL PRIMARY KEY,
+    folder_name   varchar(128),
+    discipline_id INTEGER REFERENCES Discipline ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE File
@@ -29,7 +29,7 @@ CREATE TABLE File
     file_name   varchar(128),
     file_path   varchar(128),
     upload_time timestamp,
-    folder_id REFERENCES Folder ON DELETE CASCADE ON UPDATE CASCADE
+    folder_id   INTEGER REFERENCES Folder ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
