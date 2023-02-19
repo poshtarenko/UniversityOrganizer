d72d42bd827a382639544c5c9f83ef7c0db01c1c
Yevhenii <damasska13@gmail.com>
2023-02-19 15:44:21 +0200

    Rusya

---
+++ src/main/java/com/nyanband/university_organizer/entity/Ruslan.java
@@ -0,0 +1,17 @@
+package com.nyanband.university_organizer.entity;
+
+public class Ruslan {
+    String rusya;
+
+    public Ruslan(String rusya) {
+        this.rusya = rusya;
+    }
+
+    public String getRusya() {
+        return rusya;
+    }
+
+    public void setRusya(String rusya) {
+        this.rusya = rusya;
+    }
+}
