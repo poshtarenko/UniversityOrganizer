276c5b262199590fb2e05d9e3f9aae6a3e989749
poshtarenko <catemperor228@gmail.com>
2023-02-19 16:58:32 +0200

    init

--- src/main/java/com/nyanband/university_organizer/repository/ddl.sql
+++ src/main/java/com/nyanband/university_organizer/repository/ddl.sql
@@ -1,21 +1,31 @@
+CREATE TABLE Users
+(
+    id       BIGSERIAL PRIMARY KEY,
+    email    varchar(128),
+    password varchar(128)
+);
+
 CREATE TABLE Course
 (
     id      BIGSERIAL PRIMARY KEY,
     num     int,
-    user_id INTEGER REFERENCES User ON DELETE CASCADE ON UPDATE CASCADE
+    user_id INTEGER REFERENCES Users ON DELETE CASCADE ON UPDATE CASCADE
 );
+
 CREATE TABLE Semester
 (
     id        BIGSERIAL PRIMARY KEY,
     num       int,
     course_id INTEGER REFERENCES Course ON DELETE CASCADE ON UPDATE CASCADE
 );
+
 CREATE TABLE Discipline
 (
     id              BIGSERIAL PRIMARY KEY,
     discipline_name varchar(128),
     semester_id     INTEGER REFERENCES Semester ON DELETE CASCADE ON UPDATE CASCADE
 );
+
 CREATE TABLE Folder
 (
     id            BIGSERIAL PRIMARY KEY,
@@ -32,11 +42,3 @@ CREATE TABLE File
     folder_id   INTEGER REFERENCES Folder ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-
-
-CREATE TABLE User
-(
-    id       BIGSERIAL PRIMARY KEY,
-    email    varchar(128),
-    password varchar(128)
-);
