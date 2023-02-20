31963b7bf207b1ff9aca92e7134f443e55cf99cc
poshtarenko <catemperor228@gmail.com>
2023-02-21 01:20:37 +0200

    deploy

--- src/main/java/com/nyanband/university_organizer/UniversityOrganizerApplication.java
+++ src/main/java/com/nyanband/university_organizer/UniversityOrganizerApplication.java
@@ -2,9 +2,9 @@ package com.nyanband.university_organizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class UniversityOrganizerApplication {
 
     public static void main(String[] args) {
