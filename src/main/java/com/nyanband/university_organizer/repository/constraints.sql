ace489c3483d3e6017f878491d53c3b252c2d417
Yevhenii <damasska13@gmail.com>
2023-02-28 12:49:59 +0200

    added new constraint

--- src/main/java/com/nyanband/university_organizer/repository/constraints.sql
+++ src/main/java/com/nyanband/university_organizer/repository/constraints.sql
@@ -7,6 +7,9 @@ ALTER TABLE Roles
 --Сourse
 ALTER TABLE Course
     ADD CONSTRAINT course_number_check CHECK (num > 0 and num <= 6);
+
+ALTER TABLE course
+    ADD CONSTRAINT course_number_unique UNIQUE (num,user_id);
 --Semester
 ALTER TABLE Semester
     ADD CONSTRAINT semester_number_check CHECK (num > 0 and num <= 2);
@@ -31,3 +34,5 @@ ALTER TABLE ScheduleItem
     ADD CONSTRAINT lesson_type_enum CHECK (lesson_type in ('LECTURE', 'PRACTICE', 'LAB'));
 ALTER TABLE ScheduleItem
     ADD CONSTRAINT week_type_enum CHECK (week_type in ('PAIR', 'NON_PAIR', 'ALL'));
+DELETE FROM course where num = 1 and num  = 2;
+
